0239585e5fbd5f30dd5330ffeed7ec9dc935e405
mattullon <mattullon@gmail.com>
2023-01-19 12:32:58 -0300

    inicio de test

---
+++ index.js
@@ -0,0 +1,28 @@
+
+
+const unidad = [null,null,null,null]
+
+
+   
+        unidad.forEach(eve => {
+            
+            eve = random(100,200)
+           const unidad = eve 
+            return unidad;
+
+
+            //console.log(eve[1])
+           // console.log(or[1])
+
+
+
+
+        });
+
+console.log(unidad)
+
+
+
+function random(min,max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
