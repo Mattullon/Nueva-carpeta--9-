c2277927b5791b0aa8c1049ec5420b78d5b68205
mattullon <mattullon@gmail.com>
2023-01-26 13:06:32 -0300

    nuevo enfoque al problema, primera parte terminado, falta optimizar pero la logica esta hecha

---
+++ z.js
@@ -0,0 +1,63 @@
+function rand() {
+    
+    return Math.floor(Math.random() * (200 - 100 + 1)) + 100;
+  }
+
+let workHours = Math.floor(15 * 5 * 9 * 4);
+console.log(workHours)
+
+let tubesFluorecent = [0, 0, 0, 0];
+let tubesBroken = 0 
+let contsTubes
+let money = 24 
+//for (let unit = 0; unit < 4; unit ++ ) {
+
+    
+
+for (let i = 0; i <= workHours; i++) {
+    tubesFluorecent = [rand(),rand(),rand(),rand()]
+    tubesFluorecent.sort();
+    let firtValor= tubesFluorecent[0];
+    let secundValor= tubesFluorecent[1];
+   
+    if (workHours >= 0) {
+        workHours = workHours-firtValor
+        contsTubes+=1
+    } 
+    if (workHours >= 0) {
+        workHours = workHours-secundValor
+        contsTubes=contsTubes+1
+        
+    }
+    if (contsTubes = 2 ) {
+        tubesBroken = tubesBroken+2
+        contsTubes = 0
+    }
+    if (workHours <= firtValor) {
+        workHours = 0
+        tubesBroken = tubesBroken + 1
+    }
+    if (workHours <= secundValor) {
+        workHours = 0
+    }
+    
+        
+    
+    console.log(tubesFluorecent)
+    console.log(firtValor)
+    console.log(secundValor)
+    console.log(workHours)
+  }
+//}
+
+ console.log(tubesBroken)
+ console.log(tubesBroken*7)
+
+
+
+
+    // console.log(tubesBroken)
+    // console.log(firtValor)
+    // console.log(secundValor)
+    // console.log(workHours)
+
