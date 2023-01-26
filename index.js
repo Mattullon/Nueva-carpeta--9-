9ba2bd02e18836c7b0bb59e7b45033ec45e9307e
mattullon <mattullon@gmail.com>
2023-01-26 18:21:51 -0300

    test final !!!

--- index.js
+++ index.js
@@ -1,32 +1,59 @@
-function rand(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+function rand() {
+  return Math.floor(Math.random() * (200 - 100 + 1)) + 100;
 }
 
-const workHours = Math.floor(15 * 5 * 9 * 4);
-console.log(typeof workHours)
+let workHours = Math.floor(15 * 5 * 9 * 4); //all hours of the year
+//console.log(workHours);
+
 let tubesFluorecent = [0, 0, 0, 0];
+let tubesBroken = 0;
+let contTubes;
+let initialMoney = 28; // 7 * 4 initial money of the year
+for (let unit = 0; unit < 4; unit++) {
+  //simulation of the 4 units
 
-for (let i = 0; i < 4; i++) {
-  let tubesUnitBroken = [0, 0, 0, 0];
-  for (let j = 0; j < 4; j++) {
-    let housOfLife = rand(100, 200);
-    tubesUnitBroken[j] = Math.ceil(workHours / housOfLife);
+  workHours = 2700;
+  for (let i = 0; i <= workHours; i++) {
+    // simulation of a unit
+    tubesFluorecent = [rand(), rand(), rand(), rand()];
+    tubesFluorecent.sort(); // we sort the array
+    let firtFlu = tubesFluorecent[0];
+    let secundFlu = tubesFluorecent[1];
+    if (workHours > 100) {
+      if (workHours >= firtFlu) {
+        workHours = workHours - firtFlu;
+        tubesBroken += 1;
+        contTubes = +1;
+
+        if (workHours >= secundFlu) {
+          workHours = workHours - secundFlu;
+          tubesBroken += 1;
+          contTubes += 1;
+        }
+
+        if (workHours <= secundFlu) {
+          workHours = 0;
+        }
+        if (contTubes === 2) {
+          //  when two fluorescent lights are burned
+          initialMoney += 7;
+          contTubes = 0;
+        }
+      }
+
+      if (workHours < 99) {
+        workHours = 0;
+      }
+    }
+
+    // console.log(tubesFluorecent);
+    // console.log(firtFlu);
+    // console.log(secundFlu);
+    // console.log(workHours);
   }
-  tubesUnitBroken.sort(function (a, b) {
-    return a - b;
-  });
-  tubesFluorecent[i] = tubesUnitBroken[1]; // como se debe reemplazar desde que se averien dos tubos, se ordena el arreglo y se toma el segundo numero mas alto.
 }
 
-const totalCost = tubesFluorecent.reduce((a, b) => a + b, 0) * 7;
-const totalTubesBroken = tubesFluorecent.reduce((a, b) => a + b, 0);
-const initialCost = 28;
+console.log(`fluorescent tubes were broken in 1 year are: ${tubesBroken}`);
 console.log(
-  `Total de tubos fluorescentes rotos en un año: ${totalTubesBroken}`
-);
-console.log(
-  `Costo total de reemplazar los tubos fluorescentes en un año: 
-  ${totalCost + initialCost} USD`
+  `the money that fluorescent tubes cost the University per year per classroom is :${initialMoney} USD`
 );
