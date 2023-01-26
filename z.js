b00402410c5f864ef05f026e63dc027ae757d7d0
mattullon <mattullon@gmail.com>
2023-01-26 14:23:10 -0300

    test completo falta optimizar

--- z.js
+++ z.js
@@ -1,63 +1,52 @@
 function rand() {
-    
-    return Math.floor(Math.random() * (200 - 100 + 1)) + 100;
-  }
+  return Math.floor(Math.random() * (200 - 100 + 1)) + 100;
+}
 
 let workHours = Math.floor(15 * 5 * 9 * 4);
-console.log(workHours)
+//console.log(workHours);
 
 let tubesFluorecent = [0, 0, 0, 0];
-let tubesBroken = 0 
-let contsTubes
-let money = 24 
-//for (let unit = 0; unit < 4; unit ++ ) {
-
-    
-
-for (let i = 0; i <= workHours; i++) {
-    tubesFluorecent = [rand(),rand(),rand(),rand()]
+let tubesBroken = 0;
+let contsTubes;
+let money = 24;
+for (let unit = 0; unit < 4; unit++) {
+  workHours = 2700;
+  for (let i = 0; i <= workHours; i++) {
+    tubesFluorecent = [rand(), rand(), rand(), rand()];
     tubesFluorecent.sort();
-    let firtValor= tubesFluorecent[0];
-    let secundValor= tubesFluorecent[1];
-   
+    let firtValor = tubesFluorecent[0];
+    let secundValor = tubesFluorecent[1];
+
     if (workHours >= 0) {
-        workHours = workHours-firtValor
-        contsTubes+=1
-    } 
-    if (workHours >= 0) {
-        workHours = workHours-secundValor
-        contsTubes=contsTubes+1
-        
+      workHours = workHours - firtValor;
+      contsTubes += 1;
     }
-    if (contsTubes = 2 ) {
-        tubesBroken = tubesBroken+2
-        contsTubes = 0
+    if (workHours >= 0) {
+      workHours = workHours - secundValor;
+      contsTubes = contsTubes + 1;
+    }
+    if ((contsTubes = 2)) {
+      tubesBroken = tubesBroken + 2;
+      contsTubes = 0;
     }
     if (workHours <= firtValor) {
-        workHours = 0
-        tubesBroken = tubesBroken + 1
+      workHours = 0;
+      tubesBroken = tubesBroken + 1;
     }
     if (workHours <= secundValor) {
-        workHours = 0
+      workHours = 0;
     }
-    
-        
-    
-    console.log(tubesFluorecent)
-    console.log(firtValor)
-    console.log(secundValor)
-    console.log(workHours)
+
+    //   console.log(tubesFluorecent);
+    //   console.log(firtValor);
+    //   console.log(secundValor);
+    //   console.log(workHours);
   }
-//}
-
- console.log(tubesBroken)
- console.log(tubesBroken*7)
+}
 
 
-
-
-    // console.log(tubesBroken)
-    // console.log(firtValor)
-    // console.log(secundValor)
-    // console.log(workHours)
+console.log(`fluorescent tubes were broken in 1 year are: ${tubesBroken}`);
+console.log(
+  `the money that fluorescent tubes cost the University per year per classroom is :${tubesBroken * 7} USD`
+);
 
